23451bfccba43c32844aaa90451c84974f5f3e3e
eglalita <eglalita@gmail.com>
2018-09-25 13:41:22 +0700

    Ex05-ListActivity-2

--- app/src/main/java/com/egci428/ex_listactivitymodel/MainActivity.kt
+++ app/src/main/java/com/egci428/ex_listactivitymodel/MainActivity.kt
@@ -3,23 +3,30 @@ package com.egci428.ex_listactivitymodel
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-
+import android.util.Log
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
+
+
+    protected var data: ArrayList<Course>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data = DataProvider.getData()
-        val iterator = data.iterator()// Duplicate the data
+        data = DataProvider.getData()
 
-        val builder = StringBuilder()
-        while (iterator.hasNext()) {
-            val course = iterator.next()
-            builder.append(course.title).append("\n")
+        val courseArrayAdapter = ArrayAdapter<Course>(this, android.R.layout.simple_list_item_1, data)
+
+        list.setAdapter(courseArrayAdapter)
+        list.setOnItemClickListener{ adapterView, view, position, _ ->
+                val course = data!!.get(position)
+                displayDetail(course)
         }
+    }
 
-        courseListView.text = builder.toString()
+    private fun displayDetail(course: Course) {
+        Log.d("MainActivity", "Displaying Course: " + course.title)
     }
 }
