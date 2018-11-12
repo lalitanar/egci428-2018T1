534f305d6e1df54472cff8350ab5ee0323887e75
eglalita <eglalita@gmail.com>
2018-11-12 15:56:56 +0700

    Ex20_CurrentLocation

---
+++ app/src/androidTest/java/com/egco428/ex20_currentlocation/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.egco428.ex20_currentlocation
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.egco428.ex20_currentlocation", appContext.packageName)
+    }
+}
