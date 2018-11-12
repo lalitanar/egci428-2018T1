3d33f24dbf97c7089fe2c332ceafc6b9d86bed13
eglalita <eglalita@gmail.com>
2018-11-12 15:55:52 +0700

    Ex19_BasicMap

--- app/src/main/java/com/egci428/firedatabase/Message.kt
+++
@@ -1,8 +0,0 @@
-package com.egci428.firedatabase
-
-/**
- * Created by lalita on 21/11/2017 AD.
- */
-class Message (val id: String, val message: String, val rating: Int){
-    constructor(): this("","",0)
-}
